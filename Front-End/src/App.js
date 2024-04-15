7bdb17db21113a49b5c3f610323d51ed97f0fae3
Ali Hamza <120605168+mrhamza201015@users.noreply.github.com>
2024-04-15 17:19:19 +0200

    Initalization

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,52 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import LoginPage from'./pages/LoginPage/LoginPage';
+import AdminPage from'./pages/AdminPage/AdminPage';
 
-import React, { useState } from 'react';
-import './App.css'; // Make sure the CSS file is in the same directory
-import login_background from './login_background.png';
-const LoginPage = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Perform login logic here
-    console.log(id, password);
-  };
 
+
+function App() {
   return (
-    <div className="loginContainer">
-      <div className="brandSide">
-        <div className="slogan">
-        <img src={login_background} alt="login background"  />
-        </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/AdminPage" element={<AdminPage />} />
+        </Routes>
       </div>
-      <div className="formSide">
-        <div className="loginForm">
-          <h1>Log In</h1>
-          <div className="inputGroup">
-            <label htmlFor="id">ID</label>
-            <input
-              type="text"
-              id="id"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              placeholder="Enter Your ID"
-            />
-          </div>
-          <div className="inputGroup">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter Your Password"
-            />
-          </div>
-          <button onClick={handleLogin}>Log In</button>
-        </div>
-      </div>
-    </div>
+    </Router>
   );
-};
+}
 
-export default LoginPage;
+
+
+export default App;
+
 
---
+++ Front-End/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './LoginPage.css'; 
+import login_background from './login_background.png';
+const LoginPage = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform login logic here
+    console.log(id, password);
+  };
+
+  return (
+    <div className="loginContainer">
+      <div className="brandSide">
+        <div className="slogan">
+        <img src={login_background} alt="login background"  />
+        </div>
+      </div>
+      <div className="formSide">
+        <div className="loginForm">
+          <h1>Log In</h1>
+          <div className="inputGroup">
+            <label htmlFor="id">ID</label>
+            <input
+              type="text"
+              id="id"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              placeholder="Enter Your ID"
+            />
+          </div>
+          <div className="inputGroup">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter Your Password"
+            />
+          </div>
+          <button onClick={handleLogin}>Log In</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
