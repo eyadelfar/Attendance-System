732bec917367e02eca0360516eb37242d218e1f6
Mahmoud Ragab <104703847+MRagab2@users.noreply.github.com>
2024-04-18 19:15:04 +0200

    db Connection

---
+++ Back-End/db/dbConnection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "attendance",
+    port: "3306",
+});
+
+connection.connect((error) => {
+    if (error)
+        return console.error(error.message);
+    console.log('Connected to the MySQL server.');
+});
+
+module.exports = connection;
