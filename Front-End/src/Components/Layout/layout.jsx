bfb8a2de92eb247c002c9f103be28f4901244930
Mahmoud Ragab <104703847+MRagab2@users.noreply.github.com>
2024-04-28 01:01:16 +0300

    Update layout.jsx

--- Front-End/src/Components/Layout/layout.jsx
+++ Front-End/src/Components/Layout/layout.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // import './Layout.css';
 import Router from '../../routes/Router';
-import SideBar from '../sideBar/Sidebar'; // Capitalized component name
+import SideBar from '../sideBar/SideBar'; // Capitalized component name
 import Topbar from '../topBar/TopBar'; // Capitalized component name
 import Auth from '../../helper/Storage';
 const auth = Auth.getAuthUser();
