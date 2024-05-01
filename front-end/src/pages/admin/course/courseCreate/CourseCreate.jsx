1fa62707a19163c76f2e6b19817b888374e34257
Mahmoud Ragab <104703847+MRagab2@users.noreply.github.com>
2024-05-01 15:16:15 +0300

    CourseCreate update

--- front-end/src/pages/admin/course/courseCreate/CourseCreate.jsx
+++ front-end/src/pages/admin/course/courseCreate/CourseCreate.jsx
@@ -14,28 +14,28 @@ const CourseCreate = () => {
 //   };
 
     return (
-        <div className="account-settings-container">
-            <h1 className='main-header'>Courses</h1>      
-            <div className="create-form">
-                <div className='all-forms-col'>  
-                    <div className='all-forms'>
-                        <div className="course-header-of-form">
+        <div className="">
+            <h1 id='course-create-header'>Courses</h1>      
+            <div id="course-create-form">
+                <form id='course-create-forms-parent'>  
+                    <div id='course-create-form-child'>
+                        <div id="course-create-form-header">
                             <h1>Create Course</h1>
                         </div>
                 
-                        <div className="form-field">
+                        <div className="course-create-form-field">
                             <label >Course Name</label>
-                            <input type="text" id=""  placeholder='Enter The Course Name' />
+                            <input type="text" id="" placeholder='Enter The Course Name' />
                         </div>
 
-                        <div className="form-field-code">
+                        <div id="course-create-form-field-code">
                             <label >Course Code</label>
-                            <input type="text" id="l"  placeholder='Enter The Code' />
+                            <input type="text" id="" placeholder='Enter The Code' />
                         </div>
 
-                        <div className="form-field">
+                        <div className="course-create-form-field">
                             <label >Professor Name</label>
-                            <select className="courses-select">
+                            <select className="course-select">
                                 <option value="">Select A Professor</option>
                                 <option value="option1">Dale Robertson</option>
                                 <option value="option2">Option 2</option>
@@ -45,13 +45,13 @@ const CourseCreate = () => {
                     </div>
 
                     <div>
-                        <button id='create-course-button' onClick={handleSaveChanges}>
+                        <button id='course-create-button' onClick={handleSaveChanges}>
                             <div className='create-button-text'>
                                 Create
                             </div>
                         </button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     );
