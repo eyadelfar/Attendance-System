5b3773060fe424c30788bf93bceab1aea97b1711
bedohazem <bedohazem80@gmail.com>
2024-05-09 04:09:22 +0300

    Update AdminSettings.jsx

--- front-end/src/pages/admin/adminSettings/AdminSettings.jsx
+++ front-end/src/pages/admin/adminSettings/AdminSettings.jsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './AdminSettings.css';
 import AdminAvatar from './AdminAvatar.png';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
+
 const AdminSettings = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [fullname, setFullName] = useState('');
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [username, setUserName] = useState('');
   const [token] = useState(localStorage.getItem('token'));
   const decodedToken = jwtDecode(token);
+  const [professorData, setProfessorData] = useState({});
+  const [error, setError] = useState(null);
 
-  const handleSaveChanges = async () => {
-      await axios.put('http://localhost:4000/professors', {
-      faculty_id: decodedToken.user_id,
-      fullname: `${firstName} ${lastName}`,
-      password: newPassword
-    },{
+  useEffect(() => {
+    axios.get(`http://localhost:4000/professors/professor/${decodedToken.user_id}`, {
       headers: {
         'Authorization': token,
         'Content-Type': 'application/json'
-      }  
-    }
-  )
-  .then(response => console.log(response.data))
-  .catch(error => console.error(error));
-  };
+      }
+    })
+    .then((res) => {
+      setProfessorData(res.data);
+    })
+    .catch((error) => {
+      setError(error);
+    });
+  }, []);
+
+  const handleSaveChanges = async () => {
+    await axios.put('http://localhost:4000/professors', {
+    username,
+    faculty_id: decodedToken.user_id,
+    fullname ,
+    passwordOld: oldPassword,
+    passwordNew: newPassword
+  },{
+    headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json'
+    }  
+  }
+)
+.then(response => console.log(response.data))
+.catch(error => console.error(error));
+};
+
+
 
     return (
-      <div>
         <div className="account-settings-container">
           <h1 id='adminsetting-header'>Settings</h1>
           <h3 id='admin-container-header'>Edit Profile</h3>
@@ -38,9 +59,9 @@ const AdminSettings = () => {
               <div id="account-settings-profile">
                 <img id='admin-pic' src={AdminAvatar} alt="AdminAvatar"  />
                 <div >
-                  <h2 id="admin-name">John Doe</h2>
+                 <h2 id="admin-name">{professorData.fullname}</h2>
                 </div>
-                <h3 id="role">Admin</h3>
+                <h3 id="role">Proffesor</h3>
               </div>
                   
               <div id="admin-settings-form-parent">
@@ -56,8 +77,9 @@ const AdminSettings = () => {
                           type="text" 
                           id="firstName" 
                           className='admin-settings-input' 
-                          defaultValue="John" 
-                          placeholder='First Name'
+                          placeholder={professorData.fullname}
+                          value={fullname}
+                          onChange={(e) => setFullName(e.target.value)}
                       />
                     </div>
 
@@ -67,8 +89,9 @@ const AdminSettings = () => {
                           type="text" 
                           id="lastName" 
                           className='admin-settings-input'  
-                          defaultValue="221456" 
-                          placeholder='Username'
+                         placeholder={professorData.username}
+                          value={username}
+                          onChange={(e) => setUserName(e.target.value)}
                       />
                     </div>
 
@@ -100,79 +123,16 @@ const AdminSettings = () => {
                     <button id='admin-settings-save-button' onClick={handleSaveChanges}>
                       <div id='admin-settings-save-text'>
                           Save Changes
-                      </div>
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-        </div>
-        <div id="admin-settings-form-parent">
-          <form id='admin-settings-form-col'>  
-            <div id="admin-settings-form">
-              <div id="admin-settings-header">
-                <h1>Account Settings</h1>
-              </div>
-              <div className="admin-settings-form-field">
-                <label htmlFor="firstName">First Name:</label>
-                <input 
-                  type="text" 
-                  id="firstName" 
-                  className='admin-settings-input' 
-                  placeholder='First Name'
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </div>
-
-              <div className="admin-settings-form-field">
-                <label htmlFor="lastName">Last Name:</label>
-                <input 
-                  type="text" 
-                  id="lastName" 
-                  className='admin-settings-input'  
-                  placeholder='Last Name'
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </div>
-
-              <div className="admin-settings-form-field">
-                <label htmlFor="oldPassword">Old Password:</label>
-                <input
-                  type="password"
-                  id="oldPassword" 
-                  className='admin-settings-input' 
-                  placeholder='Old Password'
-                  value={oldPassword}
-                  onChange={(e) => setOldPassword(e.target.value)}
-                />
-              </div>
-
-              <div className="admin-settings-form-field">
-                <label htmlFor="newPassword">New Password:</label>
-                <input
-                  type="password"
-                  id="newPassword" 
-                  className='admin-settings-input' 
-                  placeholder='New Password'
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-              </div>
-            </div>
-            <div >
-              <button id='admin-settings-save-button' onClick={handleSaveChanges}>
-                <div id='admin-settings-save-text'>
-                  Save Changes
-                </div>
+                          </div>
               </button>
             </div>
           </form>
         </div>
       </div>
-    
+    </div>
   );
-};
+}
+
+
 
 export default AdminSettings;
