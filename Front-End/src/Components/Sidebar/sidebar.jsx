ff1b9b26a59a459c40479edc822789c4659aeb09
bedohazem <bedohazem80@gmail.com>
2024-05-18 04:34:07 +0300

    a

--- Front-End/src/Components/Sidebar/sidebar.jsx
+++ Front-End/src/Components/Sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './SideBar.css'; // Make sure to create a CSS file with this name
 import LogoSidebar from '../../pics/LogoSidebar.png';
 import dashboard from '../../pics/dashboard.png';
@@ -13,7 +13,8 @@ import register from '../../pics/register.png'
 const SideBar = () => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [decodedToken, setDecodedToken] = useState(null);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   React.useEffect(() => {
     if (token) {
       const decoded = JSON.parse(atob(token.split('.')[1]));
@@ -21,8 +22,25 @@ const SideBar = () => {
     }
   }, [token]);
 
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return null;
+  }
+
   const handleLogout = () => {
     localStorage.removeItem('token');
+    localStorage.removeItem('semester_id');
+    localStorage.removeItem('lecture_id');
+    localStorage.removeItem('course_id');
+    localStorage.removeItem('student_id');
+    localStorage.removeItem('faculty_id');
     setToken(null);
     window.location.href = '/Login';
   };
@@ -31,101 +49,99 @@ const SideBar = () => {
   const isRoleStudent = decodedToken && decodedToken.role === 'student';
 
   return (
-    <div className="container">
-      <aside className="sidebar">
-        {/* Assuming you have SVGs or icon fonts */}
-        <div><div className="logo"><img src={LogoSidebar} alt="LogoSidebar" /></div>
-        <nav className="nav-menu">
-          {isRoleAdmin && (
-            <a href="/dashboard" className="nav-link active">
-              <span className="icon">
-                <img src={dashboard} alt="dashboard" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Dashboard</span>
-            </a>
-          )}
-         
-           {isRoleAdmin && (
-          <a href="/CourseList" className="nav-link active">
-            <span className="icon">
-              <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
-            </span>
-            <span className="link-text">Courses</span>
-          </a>
-           )}
-          {isRoleStudent && (
-          <a href="/CourseListStudent" className="nav-link active">
-            <span className="icon">
-              <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
-            </span>
-            <span className="link-text">Courses</span>
-          </a>
-         )}
-
-          {isRoleAdmin && (
-            <a href="/StudentList" className="nav-link active">
-              <span className="icon">
-                <img src={student} alt="student" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Students</span>
-            </a>
-          )}
-          {isRoleAdmin && (
-            <a href="/ProfessorList" className="nav-link active">
-              <span className="icon">
-                <img src={professor} alt="professor" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Professors</span>
-            </a>
-          )}
-          {isRoleAdmin && (
-           <a href="/LectureList" className="nav-link active">
-              <span className="icon">
-                <img src={session} alt="session" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Lectures</span>
-            </a>
-           )}
+     <div className="container">
+     {isLoggedIn && (
+        <aside className="sidebar">
+          <div><div className="logo"><img src={LogoSidebar} alt="LogoSidebar" /></div>
+          <nav className="nav-menu">
+            {isRoleAdmin && (
+              <a href="/dashboard" className="nav-link active">
+                <span className="icon">
+                  <img src={dashboard} alt="dashboard" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Dashboard</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/CourseList" className="nav-link active">
+                <span className="icon">
+                  <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Courses</span>
+              </a>
+            )}
+            {isRoleStudent && (
+              <a href="/CourseListStudent" className="nav-link active">
+                <span className="icon">
+                  <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Courses</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/StudentList" className="nav-link active">
+                <span className="icon">
+                  <img src={student} alt="student" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Students</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/ProfessorList" className="nav-link active">
+                <span className="icon">
+                  <img src={professor} alt="professor" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Professors</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/LectureList" className="nav-link active">
+                <span className="icon">
+                  <img src={session} alt="session" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Lectures</span>
+              </a>
+            )}
             {isRoleStudent&&(
-            <a href="/LectureListStudent" className="nav-link active">
-              <span className="icon">
-                <img src={session} alt="session" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Lectures</span>
-            </a>
-           )}
+              <a href="/LectureListStudent" className="nav-link active">
+                <span className="icon">
+                  <img src={session} alt="session" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Lectures</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/register" className="nav-link active">
+                <span className="icon">
+                  <img src={register} alt="courses" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Register</span>
+              </a>
+            )}
+            {isRoleAdmin && (
+              <a href="/AdminSettings" className="nav-link active">
+                <span className="icon">
+                  <img src={settings} alt="settings" style={{ }} />
+                </span>
+                <span className="link-text">Settings</span>
+              </a>
+            )}
+            {isRoleStudent&&(
+              <a href="/StudentSettings" className="nav-link active">
+                <span className="icon">
+                  <img src={settings} alt="settings" style={{ marginRight: '15px' }} />
+                </span>
+                <span className="link-text">Settings</span>
+              </a>
+            )}
+          </nav></div>
 
-          {isRoleAdmin && (
-          <a href="/register" className="nav-link active">
-            <span className="icon">
-              <img src={register} alt="courses" style={{ marginRight: '15px' }} />
-            </span>
-            <span className="link-text">Register</span>
-          </a>
-           )}
-          {isRoleAdmin && (
-            <a href="/AdminSettings" className="nav-link active">
-              <span className="icon">
-                <img src={settings} alt="settings" style={{ }} />
-              </span>
-              <span className="link-text">Settings</span>
-            </a>
-          )}
-          {isRoleStudent&&(
-          <a href="/StudentSettings" className="nav-link active">
-              <span className="icon">
-                <img src={settings} alt="settings" style={{ marginRight: '15px' }} />
-              </span>
-              <span className="link-text">Settings</span>
-            </a>
-             )}
-        </nav></div>
-
-        <div className="logout">
-          <span className="icon"><img src={LogoutButoon} alt="LogoutButoon" style={{ marginRight: '15px' }}/></span>
-          <span className="link-text" onClick={handleLogout}>Logout</span>
-        </div>
-      </aside>
+          <div className="logout">
+            <span className="icon"><img src={LogoutButoon} alt="LogoutButoon" style={{ marginRight: '15px' }}/></span>
+            <span className="link-text" onClick={handleLogout}>Logout</span>
+          </div>
+        </aside>
+   )}
     </div>
   );
 };
--- Front-End/src/Components/topbar/topbar.jsx
+++ Front-End/src/Components/topbar/topbar.jsx
@@ -4,30 +4,39 @@ import icon from '../../pics/icon.png';
 import calendar from '../../pics/calendar.png';
 
 const Topbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [startDate, setStartDate] = useState(new Date());
   const today = new Date();
   const dd = String(today.getDate()).padStart(2);
   const mm = today.toLocaleString('default', { month: 'long' });
   const yyyy = today.getFullYear();
-  
   const formattedDate = `${dd} ${mm} , ${yyyy}`;
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return null;
+  }
+
+
 
   return (
     <div className="topbar">
-      {/* <div className="topbar-left">
-       `
-      </div> */}
-      <div className="topbar-right">
-      <img className='calendar-image' src={calendar} alt="calendar" />
-      <div className="topbar-date">{formattedDate}
-      
-      </div>
-      
-        <div className="notification-badge">
-          <img src={icon} alt="Profile" />
-          <div className="notification-name">John Doe</div>
+      {isLoggedIn && (
+        <div className="topbar-right">
+          <img className='calendar-image' src={calendar} alt="calendar" />
+          <div className="topbar-date">{formattedDate}</div>
+          <div className="notification-badge">
+            <img src={icon} alt="Profile" />
+            <div className="notification-name">John Doe</div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
