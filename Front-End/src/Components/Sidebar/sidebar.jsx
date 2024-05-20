4372f8aa94ca21d4c77b93c61cbba6f357c535c9
bedohazem <bedohazem80@gmail.com>
2024-05-20 20:40:44 +0300

    tesst

--- Front-End/src/Components/Sidebar/sidebar.jsx
+++ Front-End/src/Components/Sidebar/sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import './SideBar.css'; // Make sure to create a CSS file with this name
+import './SideBar.css';
 import LogoSidebar from '../../pics/LogoSidebar.png';
 import dashboard from '../../pics/dashboard.png';
 import courses from '../../pics/courses.png';
@@ -9,13 +9,14 @@ import session from '../../pics/session.png'
 import settings from '../../pics/settings.png'
 import LogoutButoon from '../../pics/LogoutButoon.png'
 import register from '../../pics/register.png'
+import { NavLink } from 'react-router-dom';
+
 
 const SideBar = () => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [decodedToken, setDecodedToken] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  
 
   React.useEffect(() => {
     if (token) {
@@ -39,114 +40,121 @@ const SideBar = () => {
 
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('semester_id');
-    localStorage.removeItem('lecture_id');
-    localStorage.removeItem('course_id');
-    localStorage.removeItem('student_id');
-    localStorage.removeItem('faculty_id');
-    setToken(null);
-    window.location.href = '/Login';
+      localStorage.removeItem('token');
+      localStorage.removeItem('semester_id');
+      localStorage.removeItem('lecture_id');
+      localStorage.removeItem('course_id');
+      localStorage.removeItem('student_id');
+      localStorage.removeItem('faculty_id');
+      setToken(null);
+      window.location.href = '/Login';
   };
 
   const isRoleAdmin = decodedToken && decodedToken.role === 'faculty';
   const isRoleStudent = decodedToken && decodedToken.role === 'student';
 
   return (
-     <div className="container">
-     {isLoggedIn && (
-        <aside className="sidebar">
-          <div><div className="logo"><img src={LogoSidebar} alt="LogoSidebar" /></div>
+    
+    <div className="container">
+    {isLoggedIn && (
+      <aside className="sidebar">
+        <div>
+          <div className="logo">
+            <img src={LogoSidebar} alt="LogoSidebar" />
+          </div>
           <nav className="nav-menu">
             {isRoleAdmin && (
-              <a href="/dashboard" className="nav-link active">
+              <NavLink to="/dashboard" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={dashboard} alt="dashboard" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Dashboard</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/CourseList" className="nav-link active">
+              <NavLink to="/CourseList" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Courses</span>
-              </a>
+              </NavLink>
             )}
             {isRoleStudent && (
-              <a href="/CourseListStudent" className="nav-link active">
+              <NavLink to="/CourseListStudent" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={courses} alt="courses" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Courses</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/StudentList" className="nav-link active">
+              <NavLink to="/StudentList" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={student} alt="student" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Students</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/ProfessorList" className="nav-link active">
+              <NavLink to="/ProfessorList" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={professor} alt="professor" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Professors</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/LectureList" className="nav-link active">
+              <NavLink to="/LectureList" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={session} alt="session" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Lectures</span>
-              </a>
+              </NavLink>
             )}
-            {isRoleStudent&&(
-              <a href="/LectureListStudent" className="nav-link active">
+            {isRoleStudent && (
+              <NavLink to="/LectureListStudent" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={session} alt="session" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Lectures</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/register" className="nav-link active">
+              <NavLink to="/register" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={register} alt="courses" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Register</span>
-              </a>
+              </NavLink>
             )}
             {isRoleAdmin && (
-              <a href="/AdminSettings" className="nav-link active">
+              <NavLink to="/AdminSettings" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
-                  <img src={settings} alt="settings" style={{ }} />
+                  <img src={settings} alt="settings" />
                 </span>
                 <span className="link-text">Settings</span>
-              </a>
+              </NavLink>
             )}
-            {isRoleStudent&&(
-              <a href="/StudentSettings" className="nav-link active">
+            {isRoleStudent && (
+              <NavLink to="/StudentSettings" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                 <span className="icon">
                   <img src={settings} alt="settings" style={{ marginRight: '15px' }} />
                 </span>
                 <span className="link-text">Settings</span>
-              </a>
+              </NavLink>
             )}
-          </nav></div>
-
-          <div className="logout">
-            <span className="icon"><img src={LogoutButoon} alt="LogoutButoon" style={{ marginRight: '15px' }}/></span>
-            <span className="link-text" onClick={handleLogout}>Logout</span>
-          </div>
-        </aside>
-   )}
-    </div>
+          </nav>
+        </div>
+        <div className="logout">
+          <span className="icon">
+            <img src={LogoutButoon} alt="LogoutButoon" style={{ marginRight: '15px' }} />
+          </span>
+          <span className="link-text" onClick={handleLogout}>Logout</span>
+        </div>
+      </aside>
+    )}
+  </div>
+    
   );
 };
 
